a9cb0ebfd1ccb90a3102505295d91fc6ed4e22d5
tekniklr <tekniklr@tekniklr.com>
2011-06-11 00:55:11 -0700

    Tests. Done (?) with Link model.

--- test/unit/link_test.rb
+++ test/unit/link_test.rb
@@ -1,7 +1,44 @@
 require 'test_helper'
 
 class LinkTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :links
+
+  def setup
+    @link = Link.new
+    @link.name = 'Link place'
+    @link.url = 'http://yahoo.com'
+    @link.visible = false
+  end
+
+  def test_validates_presence_of_name
+    @link.name = nil
+    assert_equal(false, @link.save)
+  end
+  
+  def test_validates_size_of_name
+    @link.name = '01234567890123456789012345678901234567890'
+    assert_equal(false, @link.save)
+  end
+  
+  def test_validates_presence_of_url
+    @link.url = nil
+    assert_equal(false, @link.save)
+  end
+  
+  def test_validates_size_of_url
+    @link.url = 'http://qwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwertyu.com'
+    assert_equal(false, @link.save)
+  end
+  
+  def test_validates_cromulence_of_url
+    @link.url = 'google.com'
+    assert_equal(false, @link.save)
+  end
+
+  def test_visible_links_should_not_show_all
+    visible = Link.get_visible
+    whuffie = visible.find_by_name('Whuffie Bank')
+    assert_equal(whuffie, nil)
+  end
+  
 end
