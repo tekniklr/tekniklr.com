53b53592122d4613a4a7effb1bc07ae09623f095
Teri Solow <tsolow@tekniklr.com>
2011-06-11 13:14:54 -0700

    Better error handling through science.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
   def validate  
     auth = request.env["omniauth.auth"]
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"])
-    user or raise "Not authorized"
+    user or raise UserNotAuthorized
     session[:user_id] = user.id
     flash[:notice] = 'Signed in!'
     redirect_to root_url
