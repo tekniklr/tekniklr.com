a7ed06c1f9c1ca954a6022dd37e8ee8f01dd863e
tekniklr <tekniklr@tekniklr.com>
2011-06-11 12:13:59 -0700

    Added function for before_filter to ensure login status.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,4 +17,8 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]  
   end
   
+  def is_admin?
+    logged_in? or raise "Not authorized"
+  end
+  
 end
