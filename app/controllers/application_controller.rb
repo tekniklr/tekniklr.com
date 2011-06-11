0d007cfc5f86f72b02d792a8f17f1b98eaaf8f3b
tekniklr <tekniklr@tekniklr.com>
2011-06-11 13:14:54 -0700

    Better error handling through science.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
+require 'exceptions'
 class ApplicationController < ActionController::Base
+  include Exceptions
   protect_from_forgery
+  
+  rescue_from Exceptions::UserNotAuthorized, :with => :user_not_authorized
+  
   helper_method :current_user
   helper_method :logged_in?
   
@@ -18,7 +23,14 @@ class ApplicationController < ActionController::Base
   end
   
   def is_admin?
-    logged_in? or raise "Not authorized"
+    logged_in? or raise UserNotAuthorized
+  end
+  
+  private
+  
+  def user_not_authorized
+    flash[:error] = "You do not have access to this page."
+    redirect_to root_url
   end
   
 end
