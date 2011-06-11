892b8c22fda27a686a54cb8fa209f0f28799cbea
tekniklr <tekniklr@tekniklr.com>
2011-06-11 01:08:59 -0700

    Tests. Done (?) with Experience model.

--- test/unit/experience_test.rb
+++ test/unit/experience_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class ExperienceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :experiences
+
+  def setup
+    @experience = Experience.new
+    @experience.title = 'Corrections officer'
+    @experience.affiliation = 'Hall of Records'
+    @experience.start_date = '2001-01-31'.to_date
+  end
+
+  def test_validates_presence_of_title
+    @experience.title = nil
+    assert_equal(false, @experience.save)
+  end
+  
+  def test_validates_presence_of_affiliation
+    @experience.affiliation = nil
+    assert_equal(false, @experience.save)
+  end
+  
+  def test_validates_presence_of_start_date
+    @experience.start_date = nil
+    assert_equal(false, @experience.save)
+  end
+  
 end
