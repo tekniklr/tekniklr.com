f9df72f213719e79f651b48b83fc4bedebb916b3
tekniklr <tekniklr@tekniklr.com>
2011-06-11 00:55:11 -0700

    Tests. Done (?) with Link model.

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,12 @@
 class Link < ActiveRecord::Base
+  validates_presence_of :name
+  validates_length_of   :name, :maximum => 36
+  
+  validates_presence_of :url
+  validates_length_of   :url, :maximum => 60
+  validates_format_of   :url, :with => URI.regexp
+
+  # will only return links where visible is set to true
+  scope :get_visible, where('visible = ?', true)
+
 end
