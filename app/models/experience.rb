56ebfdfa209adb19d77d7f543f9ce358130c3927
tekniklr <tekniklr@tekniklr.com>
2011-06-12 17:38:35 -0700

    attr_accessible, though in this case it ends up making no difference. Just for completeness.

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,4 +1,6 @@
 class Experience < ActiveRecord::Base
+  attr_accessible :start_date, :end_date, :title, :affiliation, :location, :tasks
+  
   validates_presence_of :title
   validates_presence_of :affiliation
   validates_presence_of :start_date
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,10 +1,14 @@
 class Link < ActiveRecord::Base
+  attr_accessible :name, :url  
+  
   validates_presence_of :name
   validates_length_of   :name, :maximum => 36
   
   validates_presence_of :url
   validates_length_of   :url, :maximum => 60
   validates_format_of   :url, :with => URI.regexp
+
+  default_scope :order => 'name asc'
 
   # will only return links where visible is set to true
   scope :get_visible, where('visible = ?', true)
