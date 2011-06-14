8e0bf4a21d1c80873a06953f3dd41d45ecf22a16
tekniklr <tekniklr@tekniklr.com>
2011-06-14 14:46:53 -0700

    Favorite management / admin display. Dependents not saving. (Rails 3 vs. Rails 2 docs?)

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,7 +2,7 @@ class Favorite < ActiveRecord::Base
   attr_accessible :favorite_type, :order
   
   has_many  :favorite_things, :dependent => :destroy
-  accepts_nested_attributes_for :favorite_things
+  accepts_nested_attributes_for :favorite_things, :reject_if => proc { |attributes| attributes['thing'].blank? }
   
   validates_presence_of :favorite_type
   validates_length_of   :favorite_type, :maximum => 12
@@ -10,6 +10,6 @@ class Favorite < ActiveRecord::Base
   validates_presence_of     :order
   validates_numericality_of :order
   
-  default_scope :order => "'order' asc"
+  default_scope :order => "'order' asc, favorite_type asc"
   
 end
