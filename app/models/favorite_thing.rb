f1860fec61a910f3d1280f88170a9b9a28b8bbcb
Teri Solow <tsolow@tekniklr.com>
2011-06-15 12:12:27 -0700

    allow_blank

--- app/models/favorite_thing.rb
+++ app/models/favorite_thing.rb
@@ -6,8 +6,8 @@ class FavoriteThing < ActiveRecord::Base
   validates_presence_of :thing
   validates_length_of   :thing, :maximum => 60
   
-  validates_length_of   :link, :maximum => 90,      :allow_nil => true, :if => :link?
-  validates_format_of   :link, :with => URI.regexp, :allow_nil => true, :if => :link?
+  validates_length_of   :link, :maximum => 90,      :allow_nil => true, :allow_blank => true
+  validates_format_of   :link, :with => URI.regexp, :allow_nil => true, :allow_blank => true
   
   validates_presence_of     :sort
   validates_numericality_of :sort
