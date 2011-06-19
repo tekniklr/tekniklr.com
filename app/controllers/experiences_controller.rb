8253a4cb497e29a14adc77357883bbf0de074f72
tekniklr <tekniklr@tekniklr.com>
2011-06-19 14:12:55 -0700

    Inplace editing for experiences. Bug: 'update' not using javascript action.

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -3,65 +3,72 @@ class ExperiencesController < ApplicationController
   before_filter   { |c| c.page_title 'experience management' }
   
   # GET /experiences
-  # GET /experiences.json
   def index
     @experiences = Experience.all
     @experience ||= Experience.new
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @experiences }
+    end
+  end
+
+  # GET /experiences/1
+  def show
+    @experience = Experience.find(params[:id])
+    respond_to do |format|
+      format.html # show.html.erb
+      format.js
     end
   end
 
   # GET /experiences/1/edit
   def edit
     @experience = Experience.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   # POST /experience
-  # POST /experiences.json
   def create
     @experience = Experience.new(params[:experience])
     respond_to do |format|
       if @experience.save
         flash[:notice] = 'Experience added.'
         format.html { redirect_to experiences_url }
-        format.json { render json: @experience, status: :created, location: @experience }
         format.js
       else
         flash[:error] = 'Experience not valid: '+@experience.errors.full_messages.join(', ')
         format.html { render action: "index" }
-        format.json { render json: @experience.errors, status: :unprocessable_entity }
         format.js   { render action: "error" }
       end
     end
   end
 
   # PUT /experiences/1
-  # PUT /experiences/1.json
   def update
     @experience = Experience.find(params[:id])
 
     respond_to do |format|
       if @experience.update_attributes(params[:experience])
-        format.html { redirect_to experiences_url, notice: 'Experience was successfully updated.' }
-        format.json { head :ok }
+        flash[:notice] = 'Experience was successfully updated.'
+        format.html { redirect_to experiences_url }
+        format.js
       else
+        flash[:error] = 'Experience not saved: '+@experience.errors.full_messages.join(', ')
         format.html { render action: "edit" }
-        format.json { render json: @experience.errors, status: :unprocessable_entity }
+        format.js   { render action: "update_error" }
       end
     end
   end
 
   # DELETE /experiences/1
-  # DELETE /experiences/1.json
   def destroy
     @experience = Experience.find(params[:id])
     @experience.destroy
 
     respond_to do |format|
       format.html { redirect_to experiences_url }
-      format.json { head :ok }
       format.js   { render :nothing => true }
     end
   end
