a868f025e8048672a629edcedec0ecbee65a86b3
Teri Solow <tsolow@tekniklr.com>
2011-06-19 14:02:17 -0700

    Don't need json here.

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,34 +3,28 @@ class FavoritesController < ApplicationController
   before_filter   { |c| c.page_title 'favorite management' }
    
   # GET /favorites
-  # GET /favorites.json
   def index
     @favorites = Favorite.all
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @favorites }
     end
   end
 
   # GET /favorites/1
-  # GET /favorites/1.json
   def show
     @favorite = Favorite.find(params[:id])
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json: @favorite }
     end
   end
 
   # GET /favorites/new
-  # GET /favorites/new.json
   def new
     @favorite = Favorite.new
     @favorite.sort = Favorite.all.count + 1
     1.upto(5) { |n| @favorite.favorite_things.build(:sort => n) }
     respond_to do |format|
       format.html # new.html.erb
-      format.json { render json: @favorite }
     end
   end
 
@@ -44,16 +38,13 @@ class FavoritesController < ApplicationController
   end
 
   # POST /favorites
-  # POST /favorites.json
   def create
     @favorite = Favorite.new(params[:favorite])
     respond_to do |format|
       if @favorite.save
         format.html { redirect_to @favorite, notice: 'Favorite was successfully created.' }
-        format.json { render json: @favorite, status: :created, location: @favorite }
       else
         format.html { render action: "new" }
-        format.json { render json: @favorite.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -81,28 +72,23 @@ class FavoritesController < ApplicationController
   end
 
   # PUT /favorites/1
-  # PUT /favorites/1.json
   def update
     @favorite = Favorite.find(params[:id])
     respond_to do |format|
       if @favorite.update_attributes(params[:favorite])
         format.html { redirect_to @favorite, notice: 'Favorite was successfully updated.' }
-        format.json { head :ok }
       else
         format.html { render action: "edit" }
-        format.json { render json: @favorite.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # DELETE /favorites/1
-  # DELETE /favorites/1.json
   def destroy
     @favorite = Favorite.find(params[:id])
     @favorite.destroy
     respond_to do |format|
       format.html { redirect_to favorites_url }
-      format.json { head :ok }
     end
   end
 end
