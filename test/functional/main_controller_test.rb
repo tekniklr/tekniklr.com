5616bb18fe774e152ec84ce934618e337882762a
tekniklr <tekniklr@tekniklr.com>
2011-06-19 18:40:04 -0700

    About page does stuff.

--- test/functional/main_controller_test.rb
+++ test/functional/main_controller_test.rb
@@ -5,11 +5,6 @@ class MainControllerTest < ActionController::TestCase
     get :index
     assert_response :success
   end
-
-  test "should get about" do
-    get :about
-    assert_response :success
-  end
   
   test "should get acknowledgments" do
     get :acknowledgments
