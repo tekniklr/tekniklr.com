79f257de8213a8566f2e2b18bc0235c79819d58f
tekniklr <tekniklr@tekniklr.com>
2011-06-20 00:44:52 -0700

    Small style changes. Added functionality to read tweets from wordpress.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110620030537) do
+ActiveRecord::Schema.define(:version => 20110620062521) do
 
   create_table "experiences", :force => true do |t|
     t.date     "start_date"
