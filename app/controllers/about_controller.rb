cf36e9738a73388d813b270e0ffdf0902ae1e2f5
tekniklr <tekniklr@tekniklr.com>
2011-06-19 21:03:00 -0700

    About page done?

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -8,6 +8,10 @@ class AboutController < ApplicationController
   private
   
   def collect_facets
+    @definition  ||= Facet.find_by_slug('definition')
+    @location    ||= Facet.find_by_slug('location')
+    @tech        ||= Facet.find_by_slug('tech')
+    @interests   ||= Facet.find_by_slug('interests')
     @about_links ||= Link.get_visible
     @favorites   ||= Favorite.all
   end
--- app/helpers/about_helper.rb
+++ app/helpers/about_helper.rb
@@ -1,2 +1,17 @@
 module AboutHelper
+  
+  def facet_value(value)
+    if value
+      lines = value.split("\n")
+      if lines.count > 1
+        out = lines.join("<br />\n")
+        sanitize out, :tags => %w(a strong em u br)
+      else
+        sanitize lines.first, :tags => %w(a strong em u)
+      end
+    else
+      ''
+    end
+  end
+  
 end
