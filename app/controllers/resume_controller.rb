91db89a2414432613ec9642043a615f2ba5abe8f
tekniklr <tekniklr@tekniklr.com>
2011-06-20 23:57:41 -0700

    Resume in hresume format, except professional experiences which have not yet been added. Savepoint.

--- app/controllers/resume_controller.rb
+++ app/controllers/resume_controller.rb
@@ -1,7 +1,19 @@
 class ResumeController < ApplicationController
   before_filter  { |c| c.page_title 'resume' }
+  before_filter   :collect_facets
 
   def index
+  end
+
+  private
+  
+  def collect_facets
+    @experiences  ||= Experience.all
+    @tech         ||= Facet.find_by_slug('tech')
+    @fun_dev      ||= Facet.find_by_slug('fun_dev')
+    @publications ||= Facet.find_by_slug('publications')
+    @education    ||= Facet.find_by_slug('education')
+    @activities   ||= Facet.find_by_slug('activities')
   end
 
 end
