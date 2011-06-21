97dabb748a7553fb7aee1493dfa4886ca0788a0a
Teri Solow <tsolow@tekniklr.com>
2011-06-20 23:57:41 -0700

    Resume in hresume format, except professional experiences which have not yet been added. Savepoint.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,28 @@ module ApplicationHelper
     content_for(:header) { stylesheet_link_tag(*args) }
   end
   
+  def facet_value(value, list = false, cssclass = false, csselclass = false)
+    if value
+      lines = value.split("\n")
+      if lines.count > 1
+        if list
+          cssclass ? out = "<ul class='#{cssclass}'>\n" : out = "<ul>\n"
+          lines.each do |line|
+            csselclass ? out << "<li class='#{csselclass}'>#{line}</li>\n" : out << "<li>#{line}</li>\n"
+          end
+          out << "</ul>"
+        else
+          out = lines.join("<br />\n")
+        end
+      else
+        out = "#{lines.first}"
+        csselclass and out = "<span class='#{csselclass}'>#{out}</span>"
+        cssclass and out = "<span class='#{cssclass}'>#{out}</span>"
+      end
+      sanitize out, :tags => %w(a strong em u br cite span ol ul li)
+    else
+      ''
+    end
+  end
+  
 end
