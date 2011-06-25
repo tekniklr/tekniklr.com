20dd9b078a137315b4d1cd50bcbd9ba8888befef
tekniklr <tekniklr@tekniklr.com>
2011-06-25 12:44:19 -0700

    Making this more efficient, hopefully.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,9 @@
-set :user, 'tekniklr'  # Your dreamhost account's username
+set :user, 'tekniklr'                 # Your dreamhost account's username
 set :domain, 'ps14010.dreamhost.com'  # Dreamhost servername where your account is located 
-set :project, 'tekniklr.com'  # Your application as its called in the repository
+set :project, 'tekniklr.com'          # Your application as its called in the repository
 set :application, 'new.tekniklr.com'  # Your app's location (domain or sub-domain name as setup in panel)
-set :applicationdir, "/home/#{user}/rails.tekniklr.com"  # The standard Dreamhost setup
+set :applicationdir, "/home/#{user}/rails.tekniklr.com"
+set :rails_env, 'production'
 
 # version control config
 set :scm, :git
@@ -13,17 +14,17 @@ set :branch, 'master'
 set :git_shallow_clone, 1
 set :scm_verbose, true
 set :deploy_via, :remote_cache
+set :copy_compression, :bz2
+
 
 role :web, domain
 role :app, domain
-role :db,  domain, :primary => true
+role :db,  domain, :primary => true   # Where migrations will run
 
 # deploy config
 set :deploy_to, applicationdir
-set :deploy_via, :export
 
 # additional settings
-default_run_options[:pty] = true  # Forgo errors when deploying from windows
+default_run_options[:pty] = false
 ssh_options[:forward_agent] = true
-set :chmod755, "app config db lib public vendor script script/* public/disp*"
 set :use_sudo, false
