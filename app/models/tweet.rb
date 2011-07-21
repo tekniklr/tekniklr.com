2fee17f6653549a33068d02948a24029dee70c3c
tekniklr <tekniklr@tekniklr.com>
2011-07-20 20:19:56 -0700

    Main page is much more lifestreamy.

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -20,10 +20,16 @@ class Tweet < ActiveRecord::Base
     # hashtags
     tweet.gsub!(/#([\w]+)/, '<a href="http://twitter.com/search?q=%23\1">@\1</a>')
     
+    tweet << "<footer class='tweet-footer'>"
+    
     # in reply to?
     if !self.tw_reply_username.blank? && !self.tw_reply_tweet.blank?
-      tweet << "\n<p class='tweet-reply'><a href='http://twitter.com/#{self.tw_reply_username}/statuses/#{self.tw_reply_tweet}'>in reply to @#{self.tw_reply_username}</a></p>"
+      tweet << "\n<span class='tweet-reply'><a href='http://twitter.com/#{self.tw_reply_username}/statuses/#{self.tw_reply_tweet}'>in reply to @#{self.tw_reply_username}</a></span> "
     end
+    
+    tweet << "<span class='tweet-when'><a href='http://twitter.com/tekniklr/statuses/#{self.tw_id}'>#{ActionController::Base.helpers.time_ago_in_words(self.tw_created_at)} ago</a></span>"
+    
+    tweet << "</footer>"
     
     tweet
   end
