49e6c1cd88d632e85e6c103fca83dae1f47a6b10
tekniklr <tekniklr@tekniklr.com>
2011-07-26 12:55:22 -0700

    Lists of items from RSS feeds can fade to make them more visually interesting.

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -1,2 +1,10 @@
 module MainHelper
+  
+  # will generate the closest cromulent fade size for the current step
+  # (in 10s, 0 - 100)
+  def fade_level(num_steps, step)
+    step_size = ((100/num_steps)/10).round * 10
+    step * step_size
+  end
+  
 end
