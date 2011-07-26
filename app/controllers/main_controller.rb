7fd69a4802529a21dd4fce97e6b9696f82f03618
tekniklr <tekniklr@tekniklr.com>
2011-07-26 13:17:24 -0700

    Twitter blog posts are boring. Don't show them on the front page.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -6,7 +6,8 @@ class MainController < ApplicationController
 
   def index
     @tweets      = Tweet.limit(3)
-    @post        = Rails.cache.fetch('blog_post', :expires_in => 10.minutes) { get_blog_post }
+    #@post        = Rails.cache.fetch('blog_post', :expires_in => 10.minutes) { get_blog_post }
+    @post        = get_blog_post
     @consumption = Rails.cache.fetch('consuming', :expires_in => 2.hours) { get_all_consuming}
     @music       = Rails.cache.fetch('last_fm',   :expires_in => 15.minutes) { get_last_fm }
     @xbox        = Rails.cache.fetch('xbox',      :expires_in => 2.hours) { get_xbox }
@@ -23,16 +24,26 @@ class MainController < ApplicationController
   private
   
   def get_blog_post
+    logger.debug "Fetching blog post from RSS..."
     require 'rss'
     begin
       rss = RSS::Parser.parse(open('http://tekniklr.com/wpblog/feed/').read, false)
-      rss.items.first
+      # iterate through for the first item that is not a twitter recap
+      rss.items.each do |item|
+        if match = item.title.match(/^Transmissions from/)
+          logger.debug "Skipping twitter post"
+        else
+          logger.debug "Found non-twitter post: #{item.title}"
+          return item
+        end
+      end
     rescue
       ''
     end
   end
 
   def get_all_consuming
+    logger.debug "Fetching all consuming from RSS..."
     require 'rss'
     begin
       rss = RSS::Parser.parse(open('http://www.allconsuming.net/person/tekniklr/rss').read, false)
@@ -43,6 +54,7 @@ class MainController < ApplicationController
   end
   
   def get_last_fm
+    logger.debug "Fetching last.fm from RSS..."
     require 'rss'
     begin
       rss = RSS::Parser.parse(open('http://ws.audioscrobbler.com/1.0/user/tekniklr/recenttracks.rss').read, false)
@@ -53,6 +65,7 @@ class MainController < ApplicationController
   end
 
   def get_xbox
+    logger.debug "Fetching xbox achievements from RSS..."
     require 'rss'
     begin
       rss = RSS::Parser.parse(open('http://www.trueachievements.com/myachievementfeedrss.aspx?gamerid=294291').read, false)
