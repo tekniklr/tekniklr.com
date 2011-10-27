addb10156d100b66fb038dee41f2890da111af1d
tekniklr <tekniklr@tekniklr.com>
2011-10-26 17:41:47 -0700

    Trying to fix IE lameness in navigation display

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,24 @@
 module ApplicationHelper
   
+  # haml ie/css hacks: https://github.com/sporkd/compass-html5-boilerplate/
+  def ie_tag(name=:body, attrs={}, &block)
+    attrs.symbolize_keys!
+    haml_concat("<!--[if lt IE 7]> #{ tag(name, add_class('ie6', attrs), true) } <![endif]-->".html_safe)
+    haml_concat("<!--[if IE 7]>    #{ tag(name, add_class('ie7', attrs), true) } <![endif]-->".html_safe)
+    haml_concat("<!--[if IE 8]>    #{ tag(name, add_class('ie8', attrs), true) } <![endif]-->".html_safe)
+    haml_concat("<!--[if gt IE 8]><!-->".html_safe)
+    haml_tag name, attrs do
+      haml_concat("<!--<![endif]-->".html_safe)
+      block.call
+    end
+  end
+  def ie_html(attrs={}, &block)
+    ie_tag(:html, attrs, &block)
+  end
+  def ie_body(attrs={}, &block)
+    ie_tag(:body, attrs, &block)
+  end
+  
   # can be called from any view to insert a stylesheet link
   # into the header for that page
   def stylesheet(*args)
@@ -32,4 +51,14 @@ module ApplicationHelper
     end
   end
   
+  private
+
+    def add_class(name, attrs)
+      classes = attrs[:class] || ''
+      classes.strip!
+      classes = ' ' + classes if !classes.blank?
+      classes = name + classes
+      attrs.merge(:class => classes)
+    end
+    
 end
