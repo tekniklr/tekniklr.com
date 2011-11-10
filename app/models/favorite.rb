e9a331da958971a3c547ff47de9e2a3c2750b682
tekniklr <tekniklr@tekniklr.com>
2011-11-09 17:45:23 -0800

    Can delete favorite things.

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,7 +2,7 @@ class Favorite < ActiveRecord::Base
   attr_accessible :favorite_type, :sort, :favorite_things_attributes
   
   has_many  :favorite_things, :dependent => :destroy
-  accepts_nested_attributes_for :favorite_things, :reject_if => proc { |attributes| attributes['thing'].blank? }
+  accepts_nested_attributes_for :favorite_things, :reject_if => proc { |attributes| attributes['thing'].blank? }, :allow_destroy => true
   
   validates_presence_of :favorite_type
   validates_length_of   :favorite_type, :maximum => 24
