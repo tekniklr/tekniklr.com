6717b2619b36f20e56fd407ae61f4c45606c1812
Teri Solow <tsolow@tekniklr.com>
2012-01-07 15:27:46 -0800

    Removed delicious from front page because also who cares

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -8,7 +8,6 @@ class MainController < ApplicationController
     @tweets      = Tweet.limit(3)
     @post        = Rails.cache.fetch('blog_post', :expires_in => 10.minutes) { get_blog_post }
     @consumption = Rails.cache.fetch('consuming', :expires_in => 2.hours) { get_all_consuming}
-    @bookmarks   = Rails.cache.fetch('delicious', :expires_in => 4.hours) { get_delicious }
   end
 
   def acknowledgments
@@ -46,17 +45,6 @@ class MainController < ApplicationController
     begin
       rss = RSS::Parser.parse(open('http://www.allconsuming.net/person/tekniklr/rss').read, false)
       rss.items[0..6]
-    rescue
-      ''
-    end
-  end
-  
-  def get_delicious
-    logger.debug "Fetching delicious bookmarks from RSS..."
-    require 'rss'
-    begin
-      rss = RSS::Parser.parse(open('http://feeds.delicious.com/v2/rss/tekniklr?count=6').read, false)
-      rss.items[0..2]
     rescue
       ''
     end
