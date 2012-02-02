9cd3a55dbb1083af63fdccbfb0f2f78d5139b2f4
tekniklr <tekniklr@tekniklr.com>
2012-02-01 22:57:03 -0800

    Using shoulda

--- test/functional/static_controller_test.rb
+++ test/functional/static_controller_test.rb
@@ -2,27 +2,27 @@ require 'test_helper'
 
 class StaticControllerTest < ActionController::TestCase
 
-  test "should get headincmeta" do
+  should "get headincmeta" do
     get :headincmeta_partial
     assert_response :success
   end
 
-  test "should get header" do
+  should "get header" do
     get :header_partial
     assert_response :success
   end
 
-  test "should get navigation" do
+  should "get navigation" do
     get :navigation_partial
     assert_response :success
   end
 
-  test "should get footer" do
+  should "get footer" do
     get :footer_partial
     assert_response :success
   end
 
-  test "should get pageend" do
+  should "get pageend" do
     get :pageend_partial
     assert_response :success
   end
