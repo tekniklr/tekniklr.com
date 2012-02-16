bb77ab99e2df26f300194af08b786671fb5978bd
tekniklr <tekniklr@tekniklr.com>
2012-02-15 18:18:46 -0800

    TV and anime should work now, tweak display.

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -27,10 +27,10 @@ class AboutController < ApplicationController
   end
   
   def get_things_amazon
-    things = []
+    things = {}
     @favorites.each do |favorite|
       case favorite.favorite_type
-      when "Movies" || "TV" || "Anime"
+      when "Movies", "TV", "Anime"
         amazon_type = 'DVD'
       when "Literature"
         amazon_type = 'Books'
@@ -44,7 +44,7 @@ class AboutController < ApplicationController
           amazon_data = get_amazon(thing.thing, amazon_type)
           if amazon_data
             logger.debug "Amazon product found!"
-            things[thing.id] = {
+            things[thing.thing] = {
               :image_url  => amazon_data[:image_url],
               :amazon_url => amazon_data[:amazon_url]
             }
