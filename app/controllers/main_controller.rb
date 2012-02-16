696d5f2587b79e589f8315940dcb4fc52f4f02a6
tekniklr <tekniklr@tekniklr.com>
2012-02-15 17:37:32 -0800

    Added amazon links to favorites, where applicable

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -43,13 +43,13 @@ class MainController < ApplicationController
 
   def get_getglue
     logger.debug "Fetching getglue checkins from RSS..."
-    #begin
+    begin
       parsed_items = []
       feed = Feedzirra::Feed.fetch_and_parse('http://feeds.getglue.com/checkins/0%22jFMxg4Rtl')
       items = feed.entries.uniq_by{|i| i.title}
       items.each do |item|
         logger.debug "Parsing #{item.title}..."
-        item.title.gsub!(/Teri Solow is ([A-Za-z]+) /, '')
+        item.title.gsub!(/Teri Solow is ([A-Za-z]+) (to )?/, '')
         case $1
         when "watching"
           type = 'DVD'
@@ -57,6 +57,8 @@ class MainController < ApplicationController
           type = 'Books'
         when "playing"
           type = 'VideoGames'
+        when "listening"
+          type = 'Music'
         end
         if type
           amazon = get_amazon(item.title, type)
@@ -73,30 +75,8 @@ class MainController < ApplicationController
         end
       end
       parsed_items
-    #rescue
-    #  ''
-    #end
-  end
-
-  def get_amazon(item_title, item_type)
-    logger.debug "Searching amazon for a/n #{item_type} called #{item_title}"
-    require 'amazon/aws/search'
-    begin
-      resp = Amazon::AWS.item_search(
-        item_type,
-        {
-          'Keywords'     => item_title,
-          'MinimumPrice' => '0001',
-          'MaximumPrice' => '29999'
-        }
-      )
-      item = resp.item_search_response.items.item.first
-      return {
-        :image_url  => item.small_image.url.__val__,
-        :amazon_url => item.item_links.first.item_link.first.url.__val__
-      }
     rescue
-      false
+      ''
     end
   end
 
--- app/sweepers/favorite_sweeper.rb
+++ app/sweepers/favorite_sweeper.rb
@@ -12,8 +12,12 @@ class FavoriteSweeper < ActionController::Caching::Sweeper
   def after_destroy(favorite)
      expire_cache_for(favorite)
   end
+  
   private
+  
   def expire_cache_for(favorite)
     expire_action :controller => 'about', :action => 'index'
+    Rails.cache.delete('things_amazon')
   end
+  
 end
