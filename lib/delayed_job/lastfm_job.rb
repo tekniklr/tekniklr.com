cc7be74b48bc3ff57fe27777b65f11f5e18b4ab9
tekniklr <tekniklr@tekniklr.com>
2012-02-16 18:11:30 -0800

    Added last.fm scrobbles to front page

---
+++ lib/delayed_job/lastfm_job.rb
@@ -0,0 +1,30 @@
+class DelayedJob::LastfmJob
+  require 'delayed_job/lastfm_job'
+  include DelayedJob::AmazonJob
+  
+  def perform
+    Rails.logger.debug "Fetching last.fm scrobbles from RSS..."
+    parsed_items = []
+    begin
+      feed = Feedzirra::Feed.fetch_and_parse('http://ws.audioscrobbler.com/1.0/user/tekniklr/recenttracks.rss')
+      items = feed.entries.uniq_by{|i| i.title}[0..10]
+    rescue
+      items = []
+    end
+    items.each do |item|
+      artist = item.title.gsub(/ – .+/, '') 
+      amazon = get_amazon(artist, 'Music')
+      if amazon
+        Rails.logger.debug "Amazon product found!"
+        parsed_items << {
+          :title      => item.title,
+          :published  => item.published,
+          :image_url  => amazon[:image_url],
+          :amazon_url => amazon[:amazon_url]
+        }
+      end
+    end
+    Rails.cache.write('lastfm', parsed_items)
+  end
+  
+end
