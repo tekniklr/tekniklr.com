2b5709819e4ce358d8b831e60270a332c3b6ecff
tekniklr <tekniklr@tekniklr.com>
2012-02-18 15:58:14 -0800

    A second layer of caching, just for amazon items.

--- lib/delayed_job/amazon_job.rb
+++ lib/delayed_job/amazon_job.rb
@@ -1,24 +1,35 @@
 module DelayedJob::AmazonJob
     
   def get_amazon(item_title, item_type)
-    Rails.logger.debug "Searching amazon for #{item_type}(s) called #{item_title}"
-    require 'amazon/aws/search'
-    begin
-      resp = Amazon::AWS.item_search(
-        item_type,
-        {
-          'Keywords'     => item_title,
-          'MinimumPrice' => '0001',
-          'MaximumPrice' => '29999'
+    cached_amazon_items = Rails.cache.read('amazon_items')
+    item_key = "#{item_type}|#{item_title}"
+    if cached_amazon_items && cached_amazon_items[item_key]
+      Rails.logger.debug "Cached amazon #{item_type}(s) called #{item_title} found"
+      return cached_amazon_items[item_key]
+    else
+      Rails.logger.debug "Cached item not found; searching amazon for #{item_type}(s) called #{item_title}"
+      require 'amazon/aws/search'
+      begin
+        resp = Amazon::AWS.item_search(
+          item_type,
+          {
+            'Keywords'     => item_title,
+            'MinimumPrice' => '0001',
+            'MaximumPrice' => '29999'
+          }
+        )
+        item = resp.item_search_response.items.item.first
+        amazon_values = {
+          :image_url  => item.small_image.url.__val__,
+          :amazon_url => item.item_links.first.item_link.first.url.__val__
         }
-      )
-      item = resp.item_search_response.items.item.first
-      return {
-        :image_url  => item.small_image.url.__val__,
-        :amazon_url => item.item_links.first.item_link.first.url.__val__
-      }
-    rescue
-      false
+        cached_amazon_items ||= {}
+        cached_amazon_items[item_key] = amazon_values
+        Rails.cache.write('amazon_items', cached_amazon_items)
+        return amazon_values
+      rescue
+        false
+      end
     end
   end
   
