5d6481477f613b519648d26c24bb635a394a4f4b
tekniklr <tekniklr@tekniklr.com>
2013-04-05 15:49:24 -0700

    Make front page blog post come directly from tumblr

---
+++ lib/delayed_job/tumblr_job.rb
@@ -0,0 +1,16 @@
+class DelayedJob::TumblrJob
+  require 'delayed_job/tumblr_job'
+  
+  def perform
+    Rails.logger.debug "Fetching tumblr from RSS..."
+    parsed_items = []
+    begin
+      feed  = Feedzirra::Feed.fetch_and_parse('http://tekniklr.tumblr.com/rss')
+      posts = feed.entries.uniq_by{|p| p.title}
+    rescue
+      posts = []
+    end
+    Rails.cache.write('tumblr_posts', posts)
+  end
+  
+end
