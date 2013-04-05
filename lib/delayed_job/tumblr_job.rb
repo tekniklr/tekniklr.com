3e63a7792c9b8593a8b9d89a3632f1f33f302582
tekniklr <tekniklr@tekniklr.com>
2013-04-05 15:59:22 -0700

    Remove extraneous line

--- lib/delayed_job/tumblr_job.rb
+++ lib/delayed_job/tumblr_job.rb
@@ -3,7 +3,6 @@ class DelayedJob::TumblrJob
   
   def perform
     Rails.logger.debug "Fetching tumblr from RSS..."
-    parsed_items = []
     begin
       feed  = Feedzirra::Feed.fetch_and_parse('http://tekniklr.tumblr.com/rss')
       posts = feed.entries.uniq_by{|p| p.title}
