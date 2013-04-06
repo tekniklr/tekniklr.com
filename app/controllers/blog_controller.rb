2ca47edb2bb1e53bb435d42304d6a311704a35f1
tekniklr <tekniklr@tekniklr.com>
2013-04-05 23:25:00 -0700

    Try to be more greedy about getting tumblr posts

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -5,7 +5,7 @@ class BlogController < ApplicationController
     @tumblog = true
     @tumblr_expiry   = Rails.cache.read('tumblr_expiry')
     @tumblr_posts    = Rails.cache.read('tumblr_posts')
-    if @tumblr_expiry.nil? || Time.now > @tumblr_expiry
+    if @tumblr_expiry.nil? || Time.now > @tumblr_expiry || @tumblr_posts.blank?
       @tumblr_expiry = Rails.cache.write('tumblr_expiry', (Time.now + 2.hours))
       require 'delayed_job/tumblr_job'
       Delayed::Job.enqueue(DelayedJob::TumblrJob.new)
