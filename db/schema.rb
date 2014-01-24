9caccbbc8f1edeedf1b3e089ffcf18fadac88f6a
tekniklr <tekniklr@tekniklr.com>
2014-01-24 01:28:47 -0800

    Add the number of players as a separate field, because in the wild I entered it for every game

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140124045147) do
+ActiveRecord::Schema.define(:version => 20140124091909) do
 
   create_table "delayed_jobs", :force => true do |t|
     t.integer  "priority",   :default => 0
@@ -92,6 +92,7 @@ ActiveRecord::Schema.define(:version => 20140124045147) do
     t.text     "other_info"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "players"
   end
 
   create_table "users", :force => true do |t|
