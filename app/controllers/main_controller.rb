9ac13c90069b649b25105534e409b8873abd55aa
tekniklr <tekniklr@tekniklr.com>
2014-02-17 08:12:05 -0800

    Expire flickr more often

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -22,7 +22,7 @@ class MainController < ApplicationController
     @flickr_expiry   = Rails.cache.read('flickr_expiry')
     @flickr_photos   = Rails.cache.read('flickr_photos')
     if @flickr_expiry.nil? || Time.now > @flickr_expiry
-      @flickr_expiry = Rails.cache.write('flickr_expiry', (Time.now + 6.hours))
+      @flickr_expiry = Rails.cache.write('flickr_expiry', (Time.now + 3.hours))
       require 'delayed_job/flickr_job'
       Delayed::Job.enqueue(DelayedJob::FlickrJob.new)
     end
