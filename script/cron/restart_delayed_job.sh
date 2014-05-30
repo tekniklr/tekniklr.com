6fa94ebb0f058011954739ad327d23084d22091b
tekniklr <tsolow@tekniklr.com>
2014-05-30 11:50:14 -0700

    Allow cron job to actually use the correct version of ruby/rails

--- script/cron/restart_delayed_job.sh
+++ script/cron/restart_delayed_job.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 # restart delayed_job if Dreamhost has killed it (as it does)
 
-# from http://stackoverflow.com/questions/3102500/cron-tab-to-restart-my-delayed-job-server
+# modified from http://stackoverflow.com/questions/3102500/cron-tab-to-restart-my-delayed-job-server
 
-app_root=/home/tekniklr/rails.tekniklr.com/current/
+home=/home/tekniklr
+app_root=${home}/rails.tekniklr.com/current/
+
+source ${home}/.bash_profile
 
 if ! [ -s ${app_root}/tmp/pids/delayed_job.pid ]; then
   RAILS_ENV=production ${app_root}/script/delayed_job start
