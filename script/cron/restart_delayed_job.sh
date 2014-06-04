b8de839416075e9089704ebe3973e87cb5303b2e
tekniklr <tekniklr@tekniklr.com>
2014-06-04 12:40:42 -0700

    Make delayed_job cron emit useful output

--- script/cron/restart_delayed_job.sh
+++ script/cron/restart_delayed_job.sh
@@ -6,8 +6,9 @@
 home=/home/tekniklr
 app_root=${home}/rails.tekniklr.com/current/
 
-source ${home}/.bash_profile
+source ${home}/.bash_profile > /dev/null 2>&1
 
 if ! [ -s ${app_root}/tmp/pids/delayed_job.pid ]; then
   RAILS_ENV=production ${app_root}/script/delayed_job start
+  echo "Restarted delayed_job!"
 fi
