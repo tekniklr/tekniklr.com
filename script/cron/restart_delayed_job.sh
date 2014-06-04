2eff5bf85501799fecf138f8dd3a919f10c7c404
tekniklr <tsolow@tekniklr.com>
2014-06-04 12:47:34 -0700

    Try to keep multiple delayed_job processes from running and clobbering each other

--- script/cron/restart_delayed_job.sh
+++ script/cron/restart_delayed_job.sh
@@ -9,6 +9,11 @@ app_root=${home}/rails.tekniklr.com/current/
 source ${home}/.bash_profile > /dev/null 2>&1
 
 if ! [ -s ${app_root}/tmp/pids/delayed_job.pid ]; then
+  # sanity check- sometimes we get multiple delayed_job processes- kill any others
+  pkill -u tekniklr -f delayed_job
+  sleep 10
+
+  # now start a new one
   RAILS_ENV=production ${app_root}/script/delayed_job start
   echo "Restarted delayed_job!"
 fi
