7ba3b0433613d5fb0e5a8715ac85ced3a41f2e24
tekniklr <tsolow@tekniklr.com>
2014-07-14 12:30:37 -0700

    Tweaked restart_delayed_job to not trust pid files

--- script/cron/restart_delayed_job.sh
+++ script/cron/restart_delayed_job.sh
@@ -5,10 +5,13 @@
 
 home=/home/tekniklr
 app_root=${home}/rails.tekniklr.com/current/
+pid_file=${app_root}/tmp/pids/delayed_job.pid
 
+# load environment
 source ${home}/.bash_profile > /dev/null 2>&1
 
-if ! [ -s ${app_root}/tmp/pids/delayed_job.pid ]; then
+# function to actually restart delayed_job
+function restart_delayed_job {
   # sanity check- sometimes we get multiple delayed_job processes- kill any others
   pkill -u tekniklr -f delayed_job
   sleep 10
@@ -16,4 +19,19 @@ if ! [ -s ${app_root}/tmp/pids/delayed_job.pid ]; then
   # now start a new one
   RAILS_ENV=production ${app_root}/script/delayed_job start
   echo "Restarted delayed_job!"
+}
+
+# does rails THINK delayed_job is running
+if [ -s  $pid_file ]; then
+  # yes - but do we actually have a process to match that pid?
+  if ! ps -p `cat $pid_file` > /dev/null 2>&1
+  then
+    # no - restart
+    rm $pid_file
+    echo "Removed dangling delayed_job pid file"
+    restart_delayed_job
+  fi
+else
+  # no - restart
+  restart_delayed_job
 fi
