c50323a6ebb3903a5915dbb03061c0cc8c1cc79e
tekniklr <tekniklr@tekniklr.com>
2014-07-28 11:25:01 -0700

    Further revisions to make  delayed_jobs hopefully restart without my intervention

--- script/cron/restart_delayed_job.sh
+++ script/cron/restart_delayed_job.sh
@@ -10,14 +10,20 @@ pid_file=${app_root}/tmp/pids/delayed_job.pid
 # load environment
 source ${home}/.bash_profile > /dev/null 2>&1
 
+# sometimes we get multiple delayed_job processes- kill any
+kill_orphaned_delayed_jobs () {
+  echo "Checking for orphaned delayed_job processes..."
+  if pgrep -u tekniklr -f delayed_job
+    echo "Orphaned jobs found! Killing..."
+    pkill -u tekniklr -f delayed_job > /dev/null 2>&1
+  else
+    echo "No oprhapned jobs found."
+  fi
+}
+
 # function to actually restart delayed_job
 restart_delayed_job () {
-  # sanity check- sometimes we get multiple delayed_job processes- kill any others
-  echo "Killing any orphaned delayed_job processes..."
-  pkill -u tekniklr -f delayed_job; true
-  sleep 10
-
-  # now start a new one
+  kill_orphaned_delayed_jobs
   echo "Starting a new delayed_job process..."
   RAILS_ENV=production ${app_root}/script/delayed_job start
   echo "Started delayed_job!"
@@ -27,6 +33,7 @@ restart_delayed_job () {
 if [ -s  $pid_file ]; then
   # yes - but do we actually have a process to match that pid?
   if ! ps -p `cat $pid_file` > /dev/null 2>&1
+    # yes - we have a running delayed job task to match rails' view of reality
   then
     # no - restart
     rm $pid_file
