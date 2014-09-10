8c2530c7bb95b26de9a3552820e1378b935f047d
tekniklr <tekniklr@tekniklr.com>
2014-09-09 18:22:39 -0700

    Cyclist

--- config/initializers/tekniklr.rb
+++ config/initializers/tekniklr.rb
@@ -1,3 +1,3 @@
 EMAIL = 'tsolow@tekniklr.com'
-TAGLINE = 'cyborg, programmer, gamer, social justice warrior, miscreant'
+TAGLINE = 'cyborg, programmer, gamer, social justice warrior, cyclist, miscreant'
 NUM_FAVS = 6
