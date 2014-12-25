c16d02573581b89d3970529678a8763014701fb3
Teri Solow <tsolow@tekniklr.com>
2014-12-25 17:37:30 -0500

    Allow deleting records through nested attributes

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -97,7 +97,7 @@ class FavoritesController < ApplicationController
   private
 
   def favorite_params
-    params.require(:favorite).permit(:favorite_type, :sort, favorite_things_attributes: [:id, :thing, :link, :sort])
+    params.require(:favorite).permit(:favorite_type, :sort, favorite_things_attributes: [:id, :thing, :link, :sort, :_destroy])
   end
   
 end
