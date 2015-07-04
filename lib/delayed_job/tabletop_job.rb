c1defbffe59fecb79b1ab5266577f6fcf1c227b2
tekniklr <tekniklr@tekniklr.com>
2015-07-04 03:20:34 -0700

    Store the title as returned by Amazon

--- lib/delayed_job/tabletop_job.rb
+++ lib/delayed_job/tabletop_job.rb
@@ -7,8 +7,9 @@ class DelayedJob::TabletopJob < Struct.new(:tabletop_games)
       amazon_data = get_amazon(game.name, 'Toys')
       if amazon_data
         things[game.name] = {
-          :image_url  => amazon_data[:image_url],
-          :amazon_url => amazon_data[:amazon_url]
+          :image_url    => amazon_data[:image_url],
+          :amazon_url   => amazon_data[:amazon_url],
+          :amazon_title => amazon_data[:amazon_title]
         }
       end
     end
