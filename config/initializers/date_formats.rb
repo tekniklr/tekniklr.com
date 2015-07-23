fb1249c1260a963a3c6744895cab4037ef894a37
tekniklr <tekniklr@tekniklr.com>
2015-07-23 16:27:39 -0700

    Add/use date formats

---
+++ config/initializers/date_formats.rb
@@ -0,0 +1,8 @@
+Time::DATE_FORMATS[:default] = '%Y-%m-%d'
+Date::DATE_FORMATS[:default] = '%Y-%m-%d'
+
+Time::DATE_FORMATS[:standard] = '%Y-%m-%d'
+Date::DATE_FORMATS[:standard] = '%Y-%m-%d'
+
+Time::DATE_FORMATS[:resume_date] = '%Y/%m'
+Date::DATE_FORMATS[:resume_date] = '%Y/%m'
