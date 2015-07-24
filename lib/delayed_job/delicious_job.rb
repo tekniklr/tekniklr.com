ef0c0b52c9c05800008528ed4109d03238984746
Teri Solow <tsolow@tekniklr.com>
2015-07-24 02:21:16 -0700

    Requesting a specific number of entries from the delicious RSS is broken

--- lib/delayed_job/delicious_job.rb
+++ lib/delayed_job/delicious_job.rb
@@ -3,8 +3,8 @@ class DelayedJob::DeliciousJob
   def perform
     Rails.logger.debug "Fetching delicious bookmarks from RSS..."
     begin
-      feed      = Feedjira::Feed.fetch_and_parse('http://feeds.delicious.com/v2/rss/tekniklr?count=4')
-      bookmarks = feed.entries[0..4]
+      feed      = Feedjira::Feed.fetch_and_parse('http://feeds.delicious.com/v2/rss/tekniklr')
+      bookmarks = feed.entries[0..3]
     rescue
       bookmarks = []
     end
