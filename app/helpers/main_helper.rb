04bf06271d608faf0bab8025132c5efe96e49428
tekniklr <tekniklr@tekniklr.com>
2015-07-23 18:34:01 -0700

    If num_steps was greater than 10, this didn't actually work at all

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -3,6 +3,7 @@ module MainHelper
   # will generate the closest cromulent fade size for the current step
   # (in 10s, 0 - 100)
   def fade_level(num_steps, step)
+    num_steps = (num_steps > 10) ? 10 : num_steps
     step_size = ((100/num_steps)/10).round * 10
     step * step_size
   end
