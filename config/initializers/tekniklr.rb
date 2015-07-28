84f63d54564b770a8cca3547ee4df19f668cb208
tekniklr <tekniklr@tekniklr.com>
2015-07-28 13:11:17 -0700

    Add cyclist to profile

--- config/initializers/tekniklr.rb
+++ config/initializers/tekniklr.rb
@@ -1,5 +1,5 @@
 EMAIL = 'tsolow@tekniklr.com'
-TAGLINE = 'cyborg, programmer, gamer, miscreant, INTJ'
+TAGLINE = 'cyborg, programmer, gamer, cyclist, miscreant, INTJ'
 NUM_FAVS = 6
 SIMILAR_THRESHOLD = 50
 CACHED_ITEMS = [
