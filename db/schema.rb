7b79f6495d061dc1a946385958d19826095d142a
Teri Solow <tsolow@tekniklr.com>
2016-01-21 20:58:39 -0800

    add an amazon url override for recent games, since if we are uploading an image there will be no chance to fetch the url in the usual way

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150723222207) do
+ActiveRecord::Schema.define(version: 20160122044244) do
 
   create_table "delayed_jobs", force: :cascade do |t|
     t.integer  "priority",   limit: 4,     default: 0
@@ -98,6 +98,7 @@ ActiveRecord::Schema.define(version: 20150723222207) do
     t.datetime "image_updated_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "amazon_url",         limit: 255
   end
 
   create_table "tabletop_games", force: :cascade do |t|
