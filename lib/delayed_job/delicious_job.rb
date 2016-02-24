5137e1fae3893e67d2ebb495887340e009ede761
tekniklr <tekniklr@tekniklr.com>
2016-02-24 15:17:21 -0800

    add an explanation for the kludge in the last commit to save future me a mystery

--- lib/delayed_job/delicious_job.rb
+++ lib/delayed_job/delicious_job.rb
@@ -1,6 +1,13 @@
 class DelayedJob::DeliciousJob
   
   def perform
+    # wtf/explanation:
+    # the delicious CDN is serving very out of date files to my web host's 
+    # server, whereas I can easily get a current file. SO, I've set up a 
+    # script on my home computer to wget the rss file and then scp it to my 
+    # web host. so, here, we'll look for a local copy of the rss feed, use 
+    # that if it is available, and fall back to trying to grab a file through 
+    # the wonky CDN.
     local_rss_file = "#{Rails.root}/tmp/delicious.rss"
     if File.exists? local_rss_file
       Rails.logger.debug "Parsing local delicious bookmarks file..."
@@ -16,11 +23,13 @@ class DelayedJob::DeliciousJob
         Rails.logger.warn "Error fetching delicious feed! (#{exception.class})"
       end
     end
+
     begin
       bookmarks = feed.entries.reject{|e| e.author != 'tekniklr'}[0..3]
     rescue
       bookmarks = []
     end
+    
     Rails.cache.write('delicious', bookmarks)
   end
   
