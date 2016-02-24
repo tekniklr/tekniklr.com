449c6173e16ab47611f1664f73a33a7fb2908558
tekniklr <tekniklr@tekniklr.com>
2016-02-24 15:12:36 -0800

    parse a local copy of my delicious rss feed, if it is available

--- lib/delayed_job/delicious_job.rb
+++ lib/delayed_job/delicious_job.rb
@@ -1,9 +1,22 @@
 class DelayedJob::DeliciousJob
   
   def perform
-    Rails.logger.debug "Fetching delicious bookmarks from RSS..."
+    local_rss_file = "#{Rails.root}/tmp/delicious.rss"
+    if File.exists? local_rss_file
+      Rails.logger.debug "Parsing local delicious bookmarks file..."
+      opened_rss_file = File.open local_rss_file
+      xml = opened_rss_file.read
+      feed = Feedjira::Feed.parse xml
+      opened_rss_file.close
+    else
+      Rails.logger.debug "Fetching delicious bookmarks from RSS..."
+      begin
+        feed = Feedjira::Feed.fetch_and_parse('http://feeds.delicious.com/v2/rss/tekniklr')
+      rescue => exception
+        Rails.logger.warn "Error fetching delicious feed! (#{exception.class})"
+      end
+    end
     begin
-      feed      = Feedjira::Feed.fetch_and_parse('http://feeds.delicious.com/v2/rss/tekniklr')
       bookmarks = feed.entries.reject{|e| e.author != 'tekniklr'}[0..3]
     rescue
       bookmarks = []
