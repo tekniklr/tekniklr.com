678f59e654e2bc7cf14246037174c2cc316a7a1e
tekniklr <tekniklr@tekniklr.com>
2016-06-01 18:21:37 -0700

    normalize titles by removing special characters and excess whitespace

--- lib/delayed_job/gaming_job.rb
+++ lib/delayed_job/gaming_job.rb
@@ -70,7 +70,7 @@ class DelayedJob::GamingJob
         }
       end
     end
-    Rails.cache.write('gaming', parsed_items.uniq{ |i| [i.title.downcase, i.additional_keywords] }[0..6])
+    Rails.cache.write('gaming', parsed_items.uniq{ |i| [i.title.downcase.gsub(/\s+/, ' ').gsub(/[^\w\s]/, ''), i.additional_keywords] }[0..6])
   end
   
   private
