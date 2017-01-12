e201b62b2e8a6b6ebdf6b449a9edbeebbf5f07ed
Teri Solow <tsolow@tekniklr.com>
2017-01-11 22:20:26 -0800

    rails 5.0.1

--- test/models/link_test.rb
+++ test/models/link_test.rb
@@ -8,33 +8,33 @@ class LinkTest < ActiveSupport::TestCase
 
   should "validate presence of name" do
     @link.name = nil
-    assert_equal(false, @link.save)
+    assert_equal false, @link.save
   end
   
   should "validate size of name" do
     @link.name = '01234567890123456789012345678901234567890'
-    assert_equal(false, @link.save)
+    assert_equal false, @link.save
   end
   
   should "validate presence of url" do
     @link.url = nil
-    assert_equal(false, @link.save)
+    assert_equal false, @link.save
   end
   
   should "validate size of url" do
     @link.url = 'http://qwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwertyfghjnctfvgbjnkmcfvgjnkmcdtfvgjnkmtfvgybhjnkmftcgvbhjnkvygbhjnkmu.com'
-    assert_equal(false, @link.save)
+    assert_equal false, @link.save
   end
   
   should "validate cromulence of url" do
     @link.url = 'google.com'
-    assert_equal(false, @link.save)
+    assert_equal false, @link.save
   end
 
   should "visible links should not show all" do
     visible = Link.get_visible
     whuffie = visible.find_by_name('Whuffie Bank')
-    assert_equal(whuffie, nil)
+    assert_nil whuffie
   end
   
 end
