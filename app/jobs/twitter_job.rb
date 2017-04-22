d96b0907f63020bc6d902a7f98d333079df31cb4
tekniklr <tekniklr@tekniklr.com>
2017-04-21 20:22:12 -0700

    hey, wow, neato- I don't need to make so many API calls when I use the API properly

--- app/jobs/twitter_job.rb
+++ app/jobs/twitter_job.rb
@@ -10,24 +10,12 @@ class TwitterJob < ApplicationJob
         config.access_token        = ACCESS_TOKEN
         config.access_token_secret = ACCESS_TOKEN_SECRET
       end
-      twitter_user = twitter.user('tekniklr')
-      tweets = twitter.user_timeline('tekniklr', {count: 42, exclude_replies: true, include_rts: true, trim_user: true} )
-      twitter_avatars = {}
-      tweets.each do |tweet|
-        if tweet.retweeted?
-          retweeted_handle = tweet.user_mentions.first.screen_name
-          retweeted_user = twitter.user(retweeted_handle)
-          twitter_avatars[retweeted_handle] = retweeted_user
-        end
-      end
+      tweets = twitter.user_timeline('tekniklr', {count: 42, exclude_replies: true, include_rts: true, trim_user: false} )
     rescue
       twitter_user = {}
       tweets = []
-      twitter_avatars = {}
     end
-    Rails.cache.write('twitter_user', twitter_user)
     Rails.cache.write('tweets', tweets)
-    Rails.cache.write('twitter_avatars', twitter_avatars)
   end
   
 end
