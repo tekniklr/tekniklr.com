57236d8b7a574d26250674676898c622de54fddd
tekniklr <tsolow@tekniklr.com>
2017-05-30 11:30:03 -0700

    delicious is no longer providing rss.

--- config/initializers/tekniklr.rb
+++ config/initializers/tekniklr.rb
@@ -7,8 +7,6 @@ SIMILAR_THRESHOLD = 50
 CACHED_ITEMS = [
                   'tumblr_expiry',
                   'tumblr_posts',
-                  'delicious_expiry',
-                  'delicious',
                   'flickr_expiry',
                   'flickr_photos',
                   'goodreads_expiry',
