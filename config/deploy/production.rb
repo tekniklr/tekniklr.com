0805abc15b4f2b8d0e33aaa815b0010d074fa8e2
tekniklr <tekniklr@tekniklr.com>
2017-10-02 15:52:42 -0700

    upgrade ruby to 2.4.2, gems

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -14,7 +14,7 @@ set :linked_dirs, %w{public/wpblog public/images public/icons public/recent_game
 server 'tekniklr.com', user: 'tekniklr', roles: [:web, :app]
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.4.1'
+set :rbenv_ruby, '2.4.2'
 
 set :ssh_options, {
   :forward_agent => true
