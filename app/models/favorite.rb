533d52a514532e4f7307c7d83a562233b7d0427b
tekniklr <tekniklr@tekniklr.com>
2018-02-20 13:57:16 -0800

    add scope to demonstrate a rails 5.1.5 bug I'm experiencing in more complex databases at work

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -13,5 +13,17 @@ class Favorite < ApplicationRecord
   scope :with_type, lambda { |type|
     where(favorite_type: type)
   }
+  scope :thing_matches, lambda { |thing_name|
+    # added this scope to demonstrate a rails 5.1.5 problem I'm experiencing
+    # (in more complicated tables) at work. Favorite.with_things works as
+    # expected, and Favorite.thing_matches("x") works as expected. however,
+    # Favorite.thing_matches("x").with_things returns a completely useless
+    # ActiveRecord::Relation object as the generated SQL joins the tables
+    # twice. also, thing_matches doesn't work on its own if called with
+    # .includes(:favorite_things) and .references(:favorite_things), which was
+    # a working construction in rails 5.1.4
+    joins(:favorite_things).
+    where("UPPER(#{FavoriteThing.table_name}.thing) like ?", "%#{thing_name.upcase}%")
+  }
   
 end
