3befba5f397c0f2390479931da2337ff5950eef2
tekniklr <tekniklr@tekniklr.com>
2018-02-21 13:01:28 -0800

    realized the reason I got this to fail with the chained scope was because of the default_scope having an ambiguously named column- so the problem I'm experiencing in rails 5.1.5 with oracle at work is likely separate

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -8,22 +8,10 @@ class Favorite < ApplicationRecord
   validates_presence_of     :sort
   validates_numericality_of :sort
   
-  default_scope { order('sort asc, favorite_type asc') }
+  default_scope { order("#{Favorite.table_name}.sort asc, #{Favorite.table_name}.favorite_type asc") }
   scope :with_things, -> { includes(:favorite_things) }
   scope :with_type, lambda { |type|
     where(favorite_type: type)
-  }
-  scope :thing_matches, lambda { |thing_name|
-    # added this scope to demonstrate a rails 5.1.5 problem I'm experiencing
-    # (in more complicated tables) at work. Favorite.with_things works as
-    # expected, and Favorite.thing_matches("x") works as expected. however,
-    # Favorite.thing_matches("x").with_things returns a completely useless
-    # ActiveRecord::Relation object as the generated SQL joins the tables
-    # twice. also, thing_matches doesn't work on its own if called with
-    # .includes(:favorite_things) and .references(:favorite_things), which was
-    # a working construction in rails 5.1.4
-    joins(:favorite_things).
-    where("UPPER(#{FavoriteThing.table_name}.thing) like ?", "%#{thing_name.upcase}%")
   }
   
 end
