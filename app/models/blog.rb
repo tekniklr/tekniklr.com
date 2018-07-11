f324477d96c244f3b435564af412f1d6f540e81a
tekniklr <tsolow@tekniklr.com>
2018-07-11 12:36:01 -0700

    change the way the front page blog post works, querying the database directly instead of hitting the tumblr rss. this way all posts are eligible, not just tumblr posts. also, while there, trash any duplicate posts FeedWordPress created, because ¯\_(ツ)_/¯

---
+++ app/models/blog.rb
@@ -0,0 +1,31 @@
+class Blog < ApplicationRecord
+  # the posts table in my wordpress
+
+  self.establish_connection :wordpress
+  self.table_name = 'wp_posts'
+
+  scope :sorted, -> { order('post_date desc') }
+  scope :published, -> { where(post_status: 'publish')}
+  scope :posts, -> { where(post_type: 'post') }
+
+  def trash!
+    self.post_status = 'trash'
+    self.save(validate: false)
+  end
+
+  # I use the FeedWordPress plugin to import posts from my tumblr, but it's
+  # REAL GOOD at pulling in the same post two, three, more times. delete the
+  # dupes.
+  def self.remove_duplicates
+    posts = Blog.posts.published.sorted.limit(20)
+    previous_guid = false
+    posts.each do |post|
+      if post.guid == previous_guid
+        logger.debug "************ trashing duplicate post with guid #{post.guid}!"
+        post.trash!
+      end
+      previous_guid = post.guid
+    end
+  end
+
+end
