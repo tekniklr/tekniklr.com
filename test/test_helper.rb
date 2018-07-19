f3b8ae4caf2431f488c114ce098e957f7042d690
tekniklr <tekniklr@tekniklr.com>
2018-07-19 11:10:50 -0700

    update gems; add apparently now required configuration for shoulda

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,14 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'factory_bot_rails'
-require 'shoulda'
+require 'shoulda-matchers'
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :minitest
+    with.library :rails
+  end
+end
+require 'shoulda-context'
 
 class ActiveSupport::TestCase
 end
