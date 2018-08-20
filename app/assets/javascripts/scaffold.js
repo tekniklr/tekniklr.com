ac362245447e5f857ab9f59be3f8fa89cd50c822
tekniklr <tekniklr@tekniklr.com>
2018-08-20 11:35:01 -0700

    uglification was failing because whenI converted this from coffeescript I failed to remove the default parameters

--- app/assets/javascripts/scaffold.js
+++ app/assets/javascripts/scaffold.js
@@ -1,4 +1,6 @@
-window.notify = function(message, cssclass = 'notice', div = '#notification') {
+window.notify = function(message, cssclass, div) {
+  cssclass = (typeof cssclass !== 'undefined') ?  cssclass : 'notice';
+  div = (typeof div !== 'undefined') ?  div : '#notification';
   $(div).html(''); // if we don't do this the message 'flashes' before appearing
   $(div).hide(300);
   $(div).html('<div class="flash ' + cssclass + '">' + message + '</div>');
