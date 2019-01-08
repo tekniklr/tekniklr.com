c99222487531def38eb00bef956bc1a351c3955e
tekniklr <tekniklr@tekniklr.com>
2019-01-07 19:06:54 -0800

    add image uploads to tabletop games to not rely on amazon

--- app/models/tabletop_game.rb
+++ app/models/tabletop_game.rb
@@ -1,4 +1,11 @@
 class TabletopGame < ApplicationRecord
+
+  has_attached_file     :image,
+                        :styles => { :default => ["100x100>", :png] },
+                        :path => ":rails_root/public/tabletop_games/:attachment/:id/:style/:filename",
+                        :url => "/tabletop_games/:attachment/:id/:style/:filename"
+  validates_attachment_content_type :image, :content_type => [ 'image/png', 'image/jpg', 'image/jpeg', 'image/gif']
+
   validates_presence_of :name
   default_scope { order('name asc') }
 end
