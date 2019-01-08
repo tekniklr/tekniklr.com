5f60ba08b70422340c59c0112f20b5634d20e6c0
tekniklr <tekniklr@tekniklr.com>
2019-01-07 19:25:22 -0800

    always pull last fm images from the last fm api, never from amazon

--- app/jobs/lastfm_job.rb
+++ app/jobs/lastfm_job.rb
@@ -17,26 +17,13 @@ class LastfmJob < ApplicationJob
       played_on = item.key?('date') ? DateTime.strptime(item.date.uts, '%s') : DateTime.now
       large_image = item.image.select{|i| i['size'] == 'large'}
       image_url = large_image.blank? ? '' : large_image.first['content']
-      amazon = get_amazon("#{artist} #{album}", 'Music')
-      if amazon
-        parsed_items << {
-          title:        item.name,
-          artist:       artist,
-          published:    played_on,
-          image_url:    amazon[:image_url],
-          amazon_url:   amazon[:amazon_url],
-          amazon_title: amazon[:amazon_title],
-          similarity:   amazon[:similarity]
-        }
-      else
-        parsed_items << {
-          title:      item.name,
-          artist:     artist,
-          published:  played_on,
-          lastfm_url: item.url,
-          image_url:  image_url,
-        }
-      end
+      parsed_items << {
+        title:      item.name,
+        artist:     artist,
+        published:  played_on,
+        lastfm_url: item.url,
+        image_url:  image_url,
+      }
     end
     Rails.cache.write('lastfm', parsed_items)
   end
