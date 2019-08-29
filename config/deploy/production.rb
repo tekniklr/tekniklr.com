faee0d5947ef2312289800e6c524671268f721d7
tekniklr <tsolow@tekniklr.com>
2019-08-28 21:05:37 -0700

    ruby 2.6.4, update gems

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,7 +4,7 @@ set :deploy_to, "/home/tekniklr/rails.tekniklr.com"
 set :branch, 'master'
 set :tmp_dir, "/home/tekniklr/tmp"
 set :rbenv_type, :user
-set :rbenv_ruby, '2.6.3'
+set :rbenv_ruby, '2.6.4'
 set :ssh_options, {
   forward_agent: true
 }
