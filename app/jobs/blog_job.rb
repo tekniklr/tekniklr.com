e5dfc8eca20bb9a5fa66976dbab38340fdc7921c
tekniklr <tekniklr@tekniklr.com>
2019-10-16 14:19:47 -0700

    this will never work in dev while I'm at home because of my VPN, so don't sweat it.

--- app/jobs/blog_job.rb
+++ app/jobs/blog_job.rb
@@ -2,9 +2,14 @@ class BlogJob < ApplicationJob
   
   def perform
     Rails.logger.debug "Fetching newest blog post from database, purging duplicate posts"
-    Blog.remove_duplicates!
-    Blog.remove_old!
-    post = Blog.posts.published.sorted.first
+    begin
+      Blog.remove_duplicates!
+      Blog.remove_old!
+      post = Blog.posts.published.sorted.first
+    rescue Mysql2::Error::ConnectionError
+      Rails.logger.debug "Unable to connect to database!"
+      post = nil
+    end
     Rails.cache.write('blog_posts', post)
   end
   
