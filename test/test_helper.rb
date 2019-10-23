50143129760b1346c484e026f52e85853e8c7237
tekniklr <tekniklr@tekniklr.com>
2019-10-23 11:04:44 -0700

    be more Fabulous

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,7 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'factory_bot_rails'
 require 'shoulda-matchers'
+require 'minitest/pride'
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     with.test_framework :minitest
