64bf1dd86c7dcde85871c94fad3805f569ddea80
tekniklr <tekniklr@tekniklr.com>
2019-11-26 14:44:34 -0800

    more useful comments

--- app/jobs/twitter_job.rb
+++ app/jobs/twitter_job.rb
@@ -12,9 +12,7 @@ class TwitterJob < ApplicationJob
       end
       tweets = twitter.user_timeline('tekniklr', {count: 69, exclude_replies: false, include_rts: true, trim_user: false, tweet_mode: 'extended'} )
       if tweets.first.created_at.to_datetime < Time.now-1.week
-        # if I haven't tweeted in a while, maybe I've quit twitter? it's
-        # plausible, as twitter is only getting worse. don't show tweets, in
-        # that case.
+        # if I haven't tweeted in a while, don't show old shit
         tweets = []
       end
     rescue
