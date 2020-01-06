bc1325b1a3a79949b2f40175e4f0949dc62349b6
tekniklr <tekniklr@tekniklr.com>
2020-01-06 12:15:07 -0800

    if I don't have any recent blog posts (because tumblr is dead), don't show old shit

--- app/jobs/blog_job.rb
+++ app/jobs/blog_job.rb
@@ -6,6 +6,10 @@ class BlogJob < ApplicationJob
       Blog.remove_duplicates!
       Blog.remove_old!
       post = Blog.posts.published.sorted.first
+      if post.post_date.to_datetime < Time.now-1.week
+        # if I haven't posted in a while, don't show old shit
+        post = nil
+      end
     rescue  => exception
       ErrorMailer.background_error('caching/purging blog posts', exception).deliver_now
       post = nil
