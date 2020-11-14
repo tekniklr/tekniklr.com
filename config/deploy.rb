417fac61555a377da2bc8f5b20fcafd629b2d93d
tekniklr <tekniklr@tekniklr.com>
2020-11-14 04:10:59 -0800

    temporarily increase number of kept releases to 12 while this gets dealt with: 730ff0e99e82994da72f3254a49c78b4b2b91dd0

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@ set :application, 'tekniklr.com'
 set :repo_url,  "https://github.com/tekniklr/tekniklr.com.git"
 set :linked_files, %w{config/master.key config/database.yml}
 set :linked_dirs, %w{public/wpblog public/images public/icons public/recent_games public/favorite_things public/tabletop_games public/.well-known tmp/pids}
-set :keep_releases, 7
+set :keep_releases, 12
 
 namespace :deploy do
 
