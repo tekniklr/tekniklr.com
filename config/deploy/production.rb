ef281e0603fbf377f5eb335090ec83ee1533a177
tekniklr <tekniklr@tekniklr.com>
2021-05-07 16:07:21 -0700

    renamed master branch to main

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
 set :rails_env, 'production'
 server 'tekniklr.com', user: 'tekniklr', roles: [:web, :app]
 set :deploy_to, "/home/tekniklr/rails.tekniklr.com"
-set :branch, 'master'
+set :branch, 'main'
 set :tmp_dir, "/home/tekniklr/tmp"
 set :rbenv_type, :user
 set :rbenv_ruby, '2.7.2'
