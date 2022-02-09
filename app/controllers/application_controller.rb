3051ec5a6b3330a476b4d408f01bcf1e4c5030a6
tekniklr <tekniklr@tekniklr.com>
2022-02-09 13:43:13 -0800

    ActiveSupport::CurrentAttributes is isolated per thread, so cannot replace session in the way I was trying to make it replace session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -51,7 +51,7 @@ class ApplicationController < ActionController::Base
   end
   
   def current_user  
-    Current.user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   
   def is_admin?
