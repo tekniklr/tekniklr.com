04c3633c5041ac8e591d9130bc349707e535b8f0
tekniklr <tekniklr@tekniklr.com>
2022-02-09 18:20:06 -0800

    Is this causing my `session_expired: OmniAuth::NoSessionError`? Who knows? I only added it to shut up a firefox warning.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: '_tekniklr_com_session', same_site: :strict
+Rails.application.config.session_store :cookie_store, key: '_tekniklr_com_session', same_site: :lax
