94cbe51d531d5d5fa6d9cec4189862c4013645f1
tekniklr <tekniklr@tekniklr.com>
2022-02-21 19:09:06 -0800

    allow gravatar to load

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   config.content_security_policy do |policy|
     policy.default_src :self, :https
     policy.font_src    :self, :https, :data, 'https://fonts.gstatic.com'
-    policy.img_src     :self, :https, :data, 'https://lastfm.freetls.fastly.net', 'https://pbs.twimg.com', 'https://i.gr-assets.com', 'https://*.media.tumblr.com'
+    policy.img_src     :self, :https, :data, 'https://lastfm.freetls.fastly.net', 'https://pbs.twimg.com', 'https://i.gr-assets.com', 'https://*.media.tumblr.com', 'https://gravatar.com/avatar/*'
     policy.object_src  :none
     policy.script_src  :self, :https
     policy.style_src   :self, :https, 'https://fonts.googleapis.com'
