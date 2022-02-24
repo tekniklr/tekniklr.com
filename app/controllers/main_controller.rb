da393c4ee647276c67b35a1a00df5ae0373ad9cd
tekniklr <tekniklr@tekniklr.com>
2022-02-24 13:05:40 -0800

    all delayed_jobs were running twice because this was set up pre ActiveJob and things are different, now

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -6,35 +6,35 @@ class MainController < ApplicationController
     @tweets          = Rails.cache.read('tweets')
     if @tweet_expiry.nil? || Time.now > @tweet_expiry
       @tweet_expiry  = Rails.cache.write('tweet_expiry', (Time.now + 12.minutes))
-      Delayed::Job.enqueue(TwitterJob.new)
+      TwitterJob.perform_later
     end
 
     @blog_expiry   = Rails.cache.read('blog_expiry')
     @blog_post     = Rails.cache.read('blog_posts')
     if @blog_expiry.nil? || Time.now > @blog_expiry
       @blog_expiry = Rails.cache.write('blog_expiry', (Time.now + 12.minutes))
-      Delayed::Job.enqueue(BlogJob.new)
+      BlogJob.perform_later
     end
 
     @goodreads_expiry   = Rails.cache.read('goodreads_expiry')
     @goodreads          = Rails.cache.read('goodreads')
     if @goodreads_expiry.nil? || Time.now > @goodreads_expiry
       @goodreads_expiry = Rails.cache.write('goodreads_expiry', (Time.now + 2.hours))
-      Delayed::Job.enqueue(GoodreadsJob.new)
+      GoodreadsJob.perform_later
     end
 
     @lastfm_expiry   = Rails.cache.read('lastfm_expiry')
     @lastfm          = Rails.cache.read('lastfm')
     if @lastfm_expiry.nil? || Time.now > @lastfm_expiry
       @lastfm_expiry = Rails.cache.write('lastfm_expiry', (Time.now + 12.minutes))
-      Delayed::Job.enqueue(LastfmJob.new)
+      LastfmJob.perform_later
     end
 
     @gaming_expiry   = Rails.cache.read('gaming_expiry')
     @gaming          = Rails.cache.read('gaming')
     if @gaming_expiry.nil? || Time.now > @gaming_expiry
       @gaming_expiry = Rails.cache.write('gaming_expiry', (Time.now + 2.hours))
-      Delayed::Job.enqueue(GamingJob.new)
+      GamingJob.perform_later
     end
 
   end
