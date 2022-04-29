b79d6967354cab01a00fba1b3d8df6203d9a34f7
tekniklr <tekniklr@tekniklr.com>
2022-04-29 14:36:04 -0700

    last.fm api was being weird, returning some tracks without names ¯\_(ツ)_/¯

--- app/jobs/lastfm_job.rb
+++ app/jobs/lastfm_job.rb
@@ -7,7 +7,7 @@ class LastfmJob < ApplicationJob
       require 'lastfm'
       lastfm = Lastfm.new(Rails.application.credentials.lastfm[:api_key], Rails.application.credentials.lastfm[:api_secret])
       recent_tracks = lastfm.user.get_recent_tracks(user: 'tekniklr')
-      items = recent_tracks.uniq{|i| i.name}[0..10]
+      items = recent_tracks.uniq{|i| i.name || 'unnamed'}[0..12]
     rescue => exception
       ErrorMailer.background_error('caching Last.fm activity', exception).deliver_now
       items = []
@@ -19,7 +19,7 @@ class LastfmJob < ApplicationJob
       large_image = item.image.select{|i| i['size'] == 'large'}
       image_url = large_image.blank? ? '' : large_image.first['content']
       parsed_items << {
-        title:      item.name,
+        title:      item.has_key?('name') ? item.name : 'unnamed',
         artist:     artist,
         published:  played_on,
         lastfm_url: item.url,
