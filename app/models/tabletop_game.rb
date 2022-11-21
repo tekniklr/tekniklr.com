e148580b9618ac31fa893b9ae1f261461b5bc210
tekniklr <tekniklr@tekniklr.com>
2022-11-21 13:49:14 -0800

    allow for larger tabletop game images, improve layout

--- app/models/tabletop_game.rb
+++ app/models/tabletop_game.rb
@@ -1,9 +1,12 @@
 class TabletopGame < ApplicationRecord
 
   has_attached_file     :image,
-                        :styles => { :default => ["100x100>", :png] },
-                        :path => ":rails_root/public/tabletop_games/:attachment/:id/:style/:filename",
-                        :url => "/tabletop_games/:attachment/:id/:style/:filename"
+                        styles: {
+                          thumb: ["100x100>", :png],
+                          default: ["300x300>", :png]
+                        },
+                        path: ":rails_root/public/tabletop_games/:attachment/:id/:style/:filename",
+                        url: "/tabletop_games/:attachment/:id/:style/:filename"
   validates_attachment_content_type :image, :content_type => [ 'image/png', 'image/jpg', 'image/jpeg', 'image/gif']
 
   validates_presence_of :name
