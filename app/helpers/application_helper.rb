c877e954ac1d61d4e47e93b03887cf12ec34c9c3
tekniklr <tekniklr@tekniklr.com>
2022-12-08 10:47:28 -0800

    say 'yesterday' instead of '1 day ago'

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -30,6 +30,8 @@ module ApplicationHelper
   def how_long_ago(date)
     if date.to_date == Date.today
       return 'today'
+    elsif date.to_date == Date.yesterday
+      return 'yesterday'
     else
       return  time_ago_in_words(date)+' ago'
     end
