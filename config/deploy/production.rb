92565fdf5d823072425552be20f4520bf55d4510
tekniklr <tekniklr@tekniklr.com>
2023-09-26 16:32:34 -0700

    couldn't get rbenv to install ruby on new vps; but, rvm seems to work

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,8 +3,8 @@ server 'tekniklr.com', user: 'tekniklr', roles: [:web, :app]
 set :deploy_to, "/home/tekniklr/rails.tekniklr.com"
 set :branch, 'main'
 set :tmp_dir, "/home/tekniklr/tmp"
-set :rbenv_type, :user
-set :rbenv_ruby, '3.2.2'
+set :rvm_type, :user
+set :rvm_ruby_version, 'default'
 set :ssh_options, {
   forward_agent: true
 }
