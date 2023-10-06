24bf6c59a1d2a54b43a852b77156e13d20ad83a7
tekniklr <tekniklr@tekniklr.com>
2023-10-05 18:06:46 -0700

    fix uglifier config for rails 7.1.0, don't commit compiled assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,7 @@ Rails.application.configure do
   config.assets.digest = true 
 
   # Compress both stylesheets and JavaScripts
-  config.assets.js_compressor  = :uglifier
+  config.assets.js_compressor = Uglifier.new(harmony: true)
 
   # Specifies the header that your server uses for sending files
   # (comment out if your front-end server doesn't support this)
