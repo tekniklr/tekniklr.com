4b4b2e987c3a6cf4abd660bfc3593bb5010fa3d5
tekniklr <tekniklr@tekniklr.com>
2024-07-31 11:29:29 -0700

    remove setting that gets overwritten

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,7 +9,6 @@ Rails.application.configure do
   config.whiny_nils = true
 
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
 
   config.action_mailer.raise_delivery_errors = false
 
