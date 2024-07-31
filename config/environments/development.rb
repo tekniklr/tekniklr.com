0c3e473dc6aae46274bd85b44f25300186be08e3
tekniklr <tekniklr@tekniklr.com>
2024-07-31 11:21:34 -0700

    allow caching in dev/triage with `rails dev:cache`

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,8 +6,6 @@ Rails.application.configure do
 
   config.assets.debug = true
 
-  config.cache_classes = false
-
   config.whiny_nils = true
 
   config.consider_all_requests_local       = true
@@ -20,6 +18,20 @@ Rails.application.configure do
   config.logger = Logger.new(Rails.root.join("log",Rails.env + ".log"),1,5*1024*1024)
   
   Paperclip.options[:command_path] = "/opt/local/bin"
+
+  # Enable/disable caching. By default caching is disabled.
+  # Run rails dev:cache to toggle caching.
+  if Rails.root.join("tmp/caching-dev.txt").exist?
+    config.action_controller.perform_caching = true
+    config.action_controller.enable_fragment_cache_logging = true
+    config.cache_store = :memory_store
+    config.public_file_server.headers = {
+      "Cache-Control" => "public, max-age=#{2.days.to_i}"
+    }
+  else
+    config.action_controller.perform_caching = false
+    config.cache_store = :null_store
+  end
   
   #config.content_security_policy_report_only = true
 end
