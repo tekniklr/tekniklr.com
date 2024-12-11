fc0f10968ac4f11b1690f9d9aab73866159f9601
tekniklr <tekniklr@tekniklr.com>
2024-12-10 22:27:35 -0800

    don't retry for expected error cases, like when a steam game has no achievements

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -112,9 +112,15 @@ class ApplicationJob < ActiveJob::Base
       # something else with [this is true for the PlayStation API, apparently])
       return response['location']
     else
-      if tries <= MAX_TRIES
+      if response.is_a?(Net::HTTPBadRequest) && JSON.parse(response.body).has_key?('playerstats')
+        # the steam API really overreacts when a game has no achievements and
+        # you try to query those achievements
+        return JSON.parse(response.body)
+      elsif tries < MAX_TRIES
+        # otherwise, retry in case this is a transient error
         response = make_request(url, body: body, params: params, headers: headers, type: type, auth_token: auth_token, auth_type: auth_type, content_type: content_type, tries: tries+1)
       else
+        # other otherwise, give up
         raise "#{(response.is_a?(Hash) && response.has_key?('code')) ? response.code : 'Unanticipated'} response after #{MAX_TRIES} attempts - #{response.inspect}"
       end
     end
