5d5207fb124af6e3aeca7c1249992421ff39fa9a
tekniklr <tekniklr@tekniklr.com>
2024-12-12 19:23:01 -0800

    use PSNProfiles instead of truetrophies, as it has more history (TrueTrophies only showed limited achievements, PSNProfiles has then all); simplify the item collation when performing the gaming task, as I now can control the contents better

--- app/jobs/gaming_job.rb
+++ app/jobs/gaming_job.rb
@@ -2,93 +2,14 @@ class GamingJob < ApplicationJob
   
   def perform
     manual_items = get_recent_games
-    psn_items = get_psn_rss
+    psn_items = get_psn
     xbox_items = get_xbox
     steam_items = get_steam
-    all_items = (manual_items + psn_items + xbox_items + steam_items).sort_by{|i| i.published ? i.published : Time.now-1000.years}.reverse
-    parsed_items = []
-    cheevos_kludged = []
-    all_items.each do |item|
-      if item.respond_to?('has_key?') && item.has_key?(:parsed) # manually created RecentGame
-        title       = item.title
-        platform    = item.platform
-        published   = item.published
-        url         = item.url
-        thumb_url   = item.image.url(:thumb)
-        image_url   = item.image.url(:default)
-      elsif item.respond_to?('has_key?') && item.has_key?(:steam) # from Steam API
-        title            = item.title
-        platform         = item.platform
-        published        = item.published
-        achievement      = item.achievement
-        achievement_time = item.achievement_time
-        url              = item.url
-        image_url        = item.image ? item.image : find_game_image(title)
-        thumb_url        = item.image ? item.image : find_game_image(title, true)
-      elsif item.respond_to?('has_key?') && item.has_key?(:xbox) # from OpenXBL API
-        title            = item.title
-        platform         = item.platform
-        published        = item.published
-        achievement      = item.achievement
-        achievement_time = item.achievement_time
-        image_url        = item.image ? item.image : find_game_image(title)
-        thumb_url        = item.image ? item.image : find_game_image(title, true)
-      else # parsed from TrueTrophies RSS feeds
-        Rails.logger.debug "Parsing #{item.title}..."
-        item.title.match?(/tekniklr (started|completed)/) and next
-        item.title.match(/tekniklr won the (.*) (trophy|achievement) in (.*)\z/)
-        if ($1.blank? || $2.blank? || $3.blank?)
-          puts "Couldn't parse an achievement, type, or game title from \"#{item.title}\"! Skipping."
-          next
-        end
-        achievement = $1
-        type = $2
-        title = $3
-        case type
-        when 'trophy'
-          platform = 'Playstation'
-        when 'achievement'
-          platform = 'Xbox'
-        end
-        title.gsub!(/ Trophies/, '')
-        published = item.published
-        achievement_time = item.published
-        url = item.url
-        unless cheevos_kludged.include?(title)
-          manual_published = manual_items.select{|g| g.title == title}.first
-          if (manual_published && manual_published.published > published)
-            published = manual_published.published+5.seconds
-            cheevos_kludged << title
-          end
-        end
-        image_url = find_game_image(title)
-        thumb_url = find_game_image(title, true)
-      end
-      parsed_items << {
-        title:               title,
-        platform:            platform,
-        achievement:         achievement,
-        achievement_time:    achievement_time,
-        url:                 url,
-        published:           published,
-        thumb_url:           thumb_url,
-        image_url:           image_url
-      }
-    end
-    Rails.cache.write('gaming', parsed_items.sort_by{|i| i.published }.reverse.uniq{ |i| [i.title.downcase.gsub(/\s+/, ' ').gsub(/[^\w\s]/, '')] }[0..9])
+    all_items = (manual_items + psn_items + xbox_items + steam_items).sort_by{|i| i.published}.reverse
+    Rails.cache.write('gaming', all_items.sort_by{|i| i.published }.reverse.uniq{ |i| [i.title.downcase.gsub(/[^A-z0-9]/, '')] }[0..9])
   end
   
   private
-
-  def find_game_image(title, thumb = false)
-    Rails.logger.debug "Looking for upladed image for #{title}..."
-    matching_game = RecentGame.by_name_with_image(title).first
-    if matching_game && matching_game.image?
-      thumb ? matching_game.image.url(:thumb) : matching_game.image.url(:default)
-    else
-      ''
-    end
-  end
 
   def get_recent_games
     Rails.logger.debug "Parsing manually entered games..."
@@ -96,22 +17,48 @@ class GamingJob < ApplicationJob
     RecentGame.first(12).each do |game|
       sort_time = Time.zone.local(game.started_playing.year, game.started_playing.month, game.started_playing.day, game.updated_at.hour, game.updated_at.min, game.updated_at.sec)
       items << {
-        parsed:      true,
-        platform:    game.platform,
         title:       game.name,
+        platform:    game.platform,
         url:         game.url,
         published:   sort_time,
-        image:       game.image
+        thumb_url:   game.image.url(:thumb),
+        image_url:   game.image.url(:default)
       }
     end
     return items
   end
 
-  # using truetrophies which seems to be one of the only reliable ways to turn 
-  # PSN trophies into an RSS feed...
-  def get_psn_rss
-    Rails.logger.debug "Fetching PSN trophies from truetrophies..."
-    get_xml('https://www.truetrophies.com/friendfeedrss.aspx?gamerid=26130', 'gaming_expiry')
+  # using PSNProfiles.com
+  def get_psn
+    Rails.logger.debug "Fetching PSN trophies from PSNProfiles..."
+    items = []
+    begin
+      games = make_request('https://psnprofiles.com/tekniklr', type: 'GET', params: { ajax: 1, completion: 'all', order: 'last-played', pf: 'all' }, content_type: 'text/html', user_agent: 'Mozilla/5.0')
+      parsed_games = Nokogiri::HTML.parse(games.html)
+      parsed_games.search('tr').select{|tr| tr.search('td').size > 3}.first(9).each do |game| # don't pull rows that are doing a colspan, because those rows probably don't contain games
+        sleep 1 # avoid rate limiting
+        game_info_link = game.search('a').first['href']
+        game_info = make_request("https://psnprofiles.com#{game_info_link}", type: 'GET', params: { order: 'date' }, content_type: 'text/html', user_agent: 'Mozilla/5.0')
+        parsed_game_info = Nokogiri::HTML.parse(game_info)
+        game_title = parsed_game_info.search('h3').first.children.last.text
+        newest_achievement = parsed_game_info.search('tr').css('.completed').last
+        achievement_time = DateTime.parse("#{newest_achievement.search('td')[2].css('.typo-top-date').first.text} #{newest_achievement.search('td')[2].css('.typo-bottom-date').first.text}")
+        image = store_local_copy(parsed_game_info.search('picture').css('.game').css('.lg').search('img').first['src'], "psn_#{game_title.gsub(/[^A-z]/, '')}")
+        items << {
+            platform:         'PlayStation',
+            title:            game_title,
+            achievement:      newest_achievement.search('td')[1].children[3].text.strip.gsub(/\.\z/, ''),
+            achievement_time: achievement_time,
+            published:        achievement_time,
+            url:              "https://psnprofiles.com#{game_info_link}",
+            image_url:        image,
+            thumb_url:        image
+          }
+      end
+    rescue
+      ErrorMailer.background_error("fetching/parsing PSN activity via PSNProfiles", exception).deliver_now
+    end
+    return items
   end
 
   # using the OpenXBL API at https://xbl.io/
@@ -135,14 +82,15 @@ class GamingJob < ApplicationJob
           newest_achievement = false
           newest_achievement_time = false
         end
+        image = store_local_copy(game.displayImage, "xbox_#{game.titleId}")
         items << {
-            xbox:             true,
             platform:         'Xbox',
             title:            game.name,
             achievement:      newest_achievement ? newest_achievement.name : false,
             achievement_time: newest_achievement_time ? newest_achievement_time : false,
             published:        Time.new(game.titleHistory.lastTimePlayed),
-            image:            store_local_copy(game.displayImage, "xbox_#{game.titleId}")
+            image_url:        image,
+            thumb_url:        image
           }
       end
     rescue => exception
@@ -171,18 +119,17 @@ class GamingJob < ApplicationJob
           # wrong, just assume there are no achievements this run
           newest_achievement = false
         end
-        if newest_achievement
-          items << {
-            steam:            true,
-            platform:         'Steam',
-            title:            game.name,
-            achievement:      (newest_achievement && newest_achievement.has_key?('name')) ? newest_achievement.name : (newest_achievement ? newest_achievement.apiname : false),
-            achievement_time: newest_achievement ? Time.at(newest_achievement.unlocktime) : false,
-            published:        Time.at(game.rtime_last_played),
-            url:              "https://store.steampowered.com/app/#{game.appid}/",
-            image:            store_local_copy("https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/#{game.appid}/header.jpg", "steam_#{game.appid}")
-          }
-        end
+        image = store_local_copy("https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/#{game.appid}/header.jpg", "steam_#{game.appid}")
+        items << {
+          platform:         'Steam',
+          title:            game.name,
+          achievement:      (newest_achievement && newest_achievement.has_key?('name')) ? newest_achievement.name : (newest_achievement ? newest_achievement.apiname : false),
+          achievement_time: newest_achievement ? Time.at(newest_achievement.unlocktime) : false,
+          published:        Time.at(game.rtime_last_played),
+          url:              "https://store.steampowered.com/app/#{game.appid}/",
+          image_url:        image,
+          thumb_url:        image
+        }
       end
     rescue => exception
       ErrorMailer.background_error("fetching/parsing Steam achievements via API", exception).deliver_now
