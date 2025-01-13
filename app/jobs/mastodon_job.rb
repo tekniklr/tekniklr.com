e8aa78978d11ce64efd042d64c7df50306f2ff21
tekniklr <tekniklr@tekniklr.com>
2025-01-13 14:43:31 -0800

    disable mastodon job for now, since I'm only using bluesky and don't need to keep banging on their API

--- app/jobs/mastodon_job.rb
+++ app/jobs/mastodon_job.rb
@@ -2,18 +2,18 @@ class MastodonJob < ApplicationJob
   
   def perform
     Rails.logger.debug "Fetching mastodon via API..."
-    begin
-      client = Mastodon::REST::Client.new(base_url: Rails.application.credentials.mastodon[MASTODON][:url], bearer_token: Rails.application.credentials.mastodon[MASTODON][:access_token])
-      account = client.verify_credentials
-      toots = client.statuses(account.id, {limit: 40}).select{|t| t.attributes.visibility == 'public'} # max is 40, default is 20
-      if toots.empty? || (toots.first.created_at.to_datetime < Time.now-1.week)
-        # if I haven't tooted in a while, don't show old shit
-        toots = []
-      end
-    rescue => exception
-      ErrorMailer.background_error('caching toots', exception).deliver_now
+    # begin
+    #   client = Mastodon::REST::Client.new(base_url: Rails.application.credentials.mastodon[MASTODON][:url], bearer_token: Rails.application.credentials.mastodon[MASTODON][:access_token])
+    #   account = client.verify_credentials
+    #   toots = client.statuses(account.id, {limit: 40}).select{|t| t.attributes.visibility == 'public'} # max is 40, default is 20
+    #   if toots.empty? || (toots.first.created_at.to_datetime < Time.now-1.week)
+    #     # if I haven't tooted in a while, don't show old shit
+    #     toots = []
+    #   end
+    # rescue => exception
+    #   ErrorMailer.background_error('caching toots', exception).deliver_now
       toots = []
-    end
+    # end
     Rails.cache.write('toots', toots)
   end
   
