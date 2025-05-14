3633bd93115473d84dced4c7574619bed9de0100
tekniklr <tekniklr@tekniklr.com>
2025-05-14 10:14:34 -0700

    make extra_message on backround_error mailer a named argument

--- app/mailers/error_mailer.rb
+++ app/mailers/error_mailer.rb
@@ -3,7 +3,7 @@ class ErrorMailer < ApplicationMailer
   # regular errors are captured by exception notifier - handle rescued/etc
   # errors separately here
 
-  def background_error(message, exception, extra_message = nil)
+  def background_error(message, exception, extra_message: nil)
     @message = message
     @exception = exception
     @extra_message = extra_message
