13b386e3f956754235b8a61553c924700d083f16
tekniklr <tekniklr@tekniklr.com>
2025-05-14 10:22:51 -0700

    move defer_time to named argument on backround_error
    this way it can be kept out of email subject so emails will group together better in gmail

--- app/mailers/error_mailer.rb
+++ app/mailers/error_mailer.rb
@@ -3,9 +3,10 @@ class ErrorMailer < ApplicationMailer
   # regular errors are captured by exception notifier - handle rescued/etc
   # errors separately here
 
-  def background_error(message, exception, extra_message: nil)
+  def background_error(message, exception, defer_time: nil, extra_message: nil)
     @message = message
     @exception = exception
+    @defer_time = defer_time
     @extra_message = extra_message
     mail(
       to:      'rails@tekniklr.com',
