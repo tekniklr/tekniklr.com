d4a97ed953f8e5ad341d6fe7f088ee8dad3784cb
tekniklr <tekniklr@tekniklr.com>
2025-05-21 00:57:27 -0700

    psn seems pretty robust, so if it fails it is likely for npsso reasons; since that's a slightly more involved fix, defer for longer so if I'm out of town I just get one email a day instead of four

--- app/jobs/gaming_job.rb
+++ app/jobs/gaming_job.rb
@@ -2,7 +2,7 @@ class GamingJob < ApplicationJob
   
   def perform
     defer_retry('fetch_nintendo', 12) { get_nintendo }
-    defer_retry('fetch_psn', 6) { get_psn }
+    defer_retry('fetch_psn', 24) { get_psn }
     defer_retry('fetch_steam', 12) { get_steam }
     defer_retry('fetch_xbox', 6) { get_xbox }
     recent_games = get_recent_games
