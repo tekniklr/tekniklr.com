c21cdf9e7230223fcdf8812f1fb6253233ae8a79
tekniklr <tekniklr@tekniklr.com>
2025-08-12 09:37:03 -0700

    include defer time for error emails generated when fetching/parsing XML

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -25,16 +25,17 @@ class ApplicationJob < ActiveJob::Base
   def get_xml(url, cache = false)
     items = []
     error = false
+    defer_time = Time.now + 6.hours
     begin
       xml   = HTTParty.get(url).body
       feed  = Feedjira.parse(xml)
       items = feed.entries
     rescue Feedjira::NoParserAvailable => exception
       error = true
-      ErrorMailer.background_error("parsing XML file from #{url}", exception, extra_message: "received XML:\n\n#{xml}").deliver_now
+      ErrorMailer.background_error("parsing XML file from #{url}", exception, extra_message: "received XML:\n\n#{xml}", defer_time: defer_time).deliver_now
     rescue => exception
       error = true
-      ErrorMailer.background_error("fetching/parsing XML file from #{url}", exception).deliver_now
+      ErrorMailer.background_error("fetching/parsing XML file from #{url}", exception, defer_time: defer_time).deliver_now
     end
     if error && cache
       # if something is wrong with the external XML feed, don't keep trying it
@@ -42,7 +43,7 @@ class ApplicationJob < ActiveJob::Base
       # this is necessary because sometimes the Dreamhost server IPs get banned
       # on CloudFlare and I'll get emails about it every few hours until it
       # gets fixed. I'd like fewer error emails.
-      Rails.cache.write(cache, (Time.now + 6.hours))
+      Rails.cache.write(cache, defer_time)
     end
     return items
   end
