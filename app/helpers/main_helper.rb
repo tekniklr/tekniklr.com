b65a78395a2404c4b3da7153b80c9db54b862a4d
tekniklr <tekniklr@tekniklr.com>
2025-08-12 12:34:57 -0700

    normalize fade level by limiting the amount of items considered for calculating it, and adding a few more levels of smaller granularity

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -3,7 +3,7 @@ module MainHelper
   # will generate the closest cromulent fade size for the current step
   # there are 15 total fade styles with gradually increasing transparency
   def fade_level(num_steps, step)
-    num_steps = (num_steps > 15) ? 15 : num_steps
+    num_steps = (num_steps > 17) ? 17 : num_steps
     step_size = ((150.to_f/num_steps)/10).round * 10
     step * step_size
   end
