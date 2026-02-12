bb0bde792985cf0eb62027286fc0a89e24db20fc
tekniklr <tekniklr@tekniklr.com>
2026-02-12 14:12:55 -0800

    add methods to parse tabletop games to get min and max players from player string

--- app/models/tabletop_game.rb
+++ app/models/tabletop_game.rb
@@ -15,4 +15,31 @@ class TabletopGame < ApplicationRecord
     order('name asc')
   }
 
+  def players_min
+    players or return nil
+    match = players_regex
+    match or return nil
+    return match[1]
+  end
+
+  def players_max
+    players or return nil
+    match = players_regex
+    match or return nil
+    if match[2]
+      if match[2] == '+'
+        return nil
+      elsif (match[2] == '-') && match[3]
+        return match[3]
+      end
+    end
+    return match[1]
+  end
+
+  #private
+
+  def players_regex
+    players.match(/([0-9]+)(\-|\+)?([0-9]+)?/)
+  end
+
 end
--- test/models/tabletop_game_test.rb
+++ test/models/tabletop_game_test.rb
@@ -19,4 +19,32 @@ class TabletopGameTest < ActiveSupport::TestCase
 
   end
 
+  context "when looking at player counts" do
+
+    setup do
+      @t1 = FactoryBot.create(:tabletop_game, players: '2')
+      @t2 = FactoryBot.create(:tabletop_game, players: '2-6')
+      @t3 = FactoryBot.create(:tabletop_game, players: '7+')
+      @t4 = FactoryBot.create(:tabletop_game, players: 'something weird')
+      @t5 = FactoryBot.create(:tabletop_game, players: nil)
+    end
+
+    should "know min players if available" do
+      assert_equal '2',   @t1.players_min
+      assert_equal '2',   @t2.players_min
+      assert_equal '7',   @t3.players_min
+      assert_nil   @t4.players_min
+      assert_nil   @t5.players_min
+    end
+
+    should "know max players if available" do
+      assert_equal '2',   @t1.players_max
+      assert_equal '6',   @t2.players_max
+      assert_nil   @t3.players_max
+      assert_nil   @t4.players_max
+      assert_nil   @t5.players_max
+    end
+
+  end
+
 end
