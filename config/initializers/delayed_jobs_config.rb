9bf9efe787e27100d2ba47c033fd2db4227d7c45
tekniklr <tekniklr@tekniklr.com>
2026-02-12 13:47:05 -0800

    reconfigure DelayedJob to not retry jobs, and to purge all queued jobs in development on server start
    when developing, the job processor often would not be running unless I was working on jobs, so the same job could be queued many times, and then run repeatedly when the job processor did start. don't do that.

--- config/initializers/delayed_jobs_config.rb
+++ config/initializers/delayed_jobs_config.rb
@@ -1,4 +1,9 @@
 Delayed::Worker.destroy_failed_jobs = false
-#Delayed::Worker.sleep_delay = 60
-#Delayed::Worker.max_attempts = 3
+Delayed::Worker.max_attempts = 1
 Delayed::Worker.max_run_time = 5.minutes
+
+if Rails.env.development?
+  Delayed::Job.where(attempts: 0).each do |job|
+    job.destroy
+  end
+end
